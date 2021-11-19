52e65b82946b0227a8e027a2d9c7e6c48e113be4
Jim Myers <jim@flipsidecrypto.com>
2021-11-18 22:43:59 -0500

    Update schema

--- sql_models/models/example/terra_daily_metrics.sql
+++ sql_models/models/example/terra_daily_metrics.sql
@@ -2,7 +2,7 @@
 
 -- Terra Daily Transactions Example
 SELECT 
-    block_timestamp as metric_date,
+    metric_date,
     'tx_count' as metric_type,
     tx_count as metric_value
 -- Note we are referring to the file ./metrics/terra_daily_txs.sql
@@ -12,7 +12,7 @@ UNION ALL
 
 -- Terra Daily Blocks Example
 SELECT 
-    block_timestamp as metric_date,
+    metric_date,
     'block_count' as metric_type,
     block_count as metric_value
 -- Note we are referring to the file ./metrics/terra_daily_blocks.sql
